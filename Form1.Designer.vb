b4e4fbf85b836990e13136aa89cac4fa383fa846
Fran <caviglia80@gmail.com>
2023-08-07 12:04:21 -0300

    inicial

---
+++ Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		uploadBtn = New Button()
+		SuspendLayout()
+		' 
+		' uploadBtn
+		' 
+		uploadBtn.Location = New Point(35, 12)
+		uploadBtn.Name = "uploadBtn"
+		uploadBtn.Size = New Size(143, 26)
+		uploadBtn.TabIndex = 0
+		uploadBtn.Text = "Subir imagen"
+		uploadBtn.UseVisualStyleBackColor = True
+		' 
+		' Form1
+		' 
+		AutoScaleDimensions = New SizeF(7F, 15F)
+		AutoScaleMode = AutoScaleMode.Font
+		ClientSize = New Size(231, 45)
+		Controls.Add(uploadBtn)
+		Name = "Form1"
+		Text = "Actualizar imagen"
+		ResumeLayout(False)
+	End Sub
+
+	Friend WithEvents uploadBtn As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Text
+
+Public Class Form1
+
+	Private client As WebClient
+	Private User As String
+	Private Passw As String
+	Private ftpServer As String
+	Private secretFile As String = ""
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		CargarCredenciales()
+	End Sub
+
+	Private Sub CargarCredenciales()
+		Try
+			Dim secrets As Array = File.ReadAllLines(secretFile)
+			User = secrets(0)
+			Passw = secrets(1)
+			ftpServer = secrets(2) 'ex "ftp://c1491694.ferozo.com/public_html/"
+		Catch ex As Exception
+			MsgBox(ex.Message)
+		End Try
+	End Sub
+
+	Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles uploadBtn.Click
+		Try
+			Dim IMfile As New OpenFileDialog
+			If IMfile.ShowDialog() = DialogResult.OK Then
+				Dim ftpServer As String = "ftp://c1491694.ferozo.com/public_html/"
+
+				Using httpClient As New HttpClient()
+					httpClient.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{User}:{Passw}")))
+					Dim apiUrl As String = ftpServer + Path.GetFileName(IMfile.FileName)
+					Dim fileStream As Stream = File.OpenRead(IMfile.FileName)
+					Using streamContent As New StreamContent(fileStream)
+						Using formData As New MultipartFormDataContent()
+							formData.Add(streamContent, "file", Path.GetFileName(IMfile.FileName))
+							Dim response As HttpResponseMessage = Await httpClient.PutAsync(apiUrl, formData)
+							response.EnsureSuccessStatusCode()
+						End Using
+					End Using
+				End Using
+			End If
+		Catch ex As Exception
+			MsgBox(ex.Message)
+		End Try
+	End Sub
+
+
+
+End Class
